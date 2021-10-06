b678b811b75d27a79e7f14919039ba092001e97b
RodPy <rodneyrrspy@gmail.com>
2021-10-05 21:00:07 -0300

    Add archivos

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data={
+        'titulo': 'LPV'
+        }
+    return render_template('index.html', ** data)
+
+@app.route('/hola')
+def hola():
+    return 'Hola'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
